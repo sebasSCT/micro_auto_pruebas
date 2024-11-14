b44f82685945b21339466c5b99888eca639e53ab
SebasSCT <sebastiancarmonat.sct@gmail.com>
2024-11-14 05:12:44 -0500

    jenkins

--- reporter.js
+++ reporter.js
@@ -1,7 +1,7 @@
 const reporter = require('cucumber-html-reporter');
 
 const options = {
-  theme: 'simple',
+  theme: 'bootstrap',
   jsonFile: './reports/cucumber_report.json',
   output: './reports/cucumber_report.html',
   reportSuiteAsScenarios: true,
@@ -15,20 +15,5 @@ const options = {
     "Executed": "Local"
   }
 };
-
-const fs = require('fs');
-const path = './reports/cucumber_report.html';
-const cssLink = '<link rel="stylesheet" type="text/css" href="cssreporte.css">';
-
-// Inserta el enlace de CSS al archivo HTML
-fs.readFile(path, 'utf8', (err, data) => {
-  if (err) throw err;
-
-  const updatedHtml = data.replace('</head>', `${cssLink}</head>`);
-  fs.writeFile(path, updatedHtml, 'utf8', (err) => {
-    if (err) throw err;
-    console.log('CSS personalizado agregado al reporte.');
-  });
-});
 
 reporter.generate(options);
