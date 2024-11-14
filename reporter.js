2eec784bed0c32a939fcab1b5fda97359d8491b5
SebasSCT <sebastiancarmonat.sct@gmail.com>
2024-11-14 04:43:35 -0500

    jenkins

--- reporter.js
+++ reporter.js
@@ -16,4 +16,19 @@ const options = {
   }
 };
 
+const fs = require('fs');
+const path = './reports/cucumber_report.html';
+const cssLink = '<link rel="stylesheet" type="text/css" href="cssreporte.css">';
+
+// Inserta el enlace de CSS al archivo HTML
+fs.readFile(path, 'utf8', (err, data) => {
+  if (err) throw err;
+
+  const updatedHtml = data.replace('</head>', `${cssLink}</head>`);
+  fs.writeFile(path, updatedHtml, 'utf8', (err) => {
+    if (err) throw err;
+    console.log('CSS personalizado agregado al reporte.');
+  });
+});
+
 reporter.generate(options);
