bf8326b6bd493fb10c5790c1b21124e158129ce1
SebasSCT <sebastiancarmonat.sct@gmail.com>
2024-11-14 04:54:49 -0500

    jenkins

--- reporter.js
+++ reporter.js
@@ -1,7 +1,6 @@
 const reporter = require('cucumber-html-reporter');
 
 const options = {
-  theme: 'bootstrap',
   jsonFile: './reports/cucumber_report.json',
   output: './reports/cucumber_report.html',
   reportSuiteAsScenarios: true,
